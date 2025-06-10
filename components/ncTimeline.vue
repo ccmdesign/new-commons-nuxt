de5de57274a0d02cd8e9f25a707a97fc556eb75c
Lucas Assis Vieira <lucasav2005@hotmail.com>
2025-06-09 22:03:23 -0300

    feat: add timeline styles and faqs styles

--- components/ncTimeline.vue
+++ components/ncTimeline.vue
@@ -4,8 +4,10 @@
       <h2>Timeline</h2>
       <p>Key dates for The New Commons Challenge:</p>
       <div class="timeline__content-cards">
-        <div class="card-aux" v-for="i in 4" :key="i">
-          <h3>Timeline Card</h3>
+        <div class="card" :class="{'active': i === 1}" v-for="i in 4" :key="i">
+          <h3>April 14, 2025</h3>
+          <span>12:00 a.m. PT</span>
+          <span>Start date of accepting concept notes</span>
         </div>
       </div>
     </div>
@@ -35,6 +37,58 @@
   display: grid;
   grid-column: content-start / content-end;
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  gap: var(--base-gutter);
 }
+
+.timeline__content-cards .card {
+  position: relative;
+  padding-top: var(--space-xl-2xl);
+  display: flex;
+  flex-direction: column;
+}
+
+.timeline__content-cards .card h3, .timeline__content-cards .card span:first-of-type {
+  font-weight: 600;
+}
+
+.timeline__content-cards .card.active h3, .timeline__content-cards .card.active span:first-of-type {
+  color: var(--primary-color);
+  font-weight: 600;
+}
+
+.timeline__content-cards .card span:last-of-type {
+  font-weight: 300;
+  margin-top: var(--space-s);
+}
+
+.timeline__content-cards .card::before {
+  content: '';
+  position: absolute;
+  border-radius: 100%;
+  z-index: 20;
+  width: 15px;
+  height: 15px;
+  background: #eaeaea;
+  inset: 0;
+}
+
+.timeline__content-cards .card::after {
+content: '';
+  position: absolute;
+  border-radius: 100%;
+  z-index: 10;
+  width: 100%;
+  height: 1px;
+  background: #eaeaea;
+  inset: 0;
+  top: 7px;
+}
+
+.timeline__content-cards .card.active::before {
+  background: var(--primary-color);
+}
+
+.timeline__content-cards .card.active::after {
+  background:  linear-gradient(90deg, #1E99FF 0%, #1E99FF 33.33%, #EAEAEA 66.67%, #EAEAEA 100%);
+  
+} 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,55 @@
     <div class="hero__content | cluster | subgrid">
       <div class="panel">
         <h1>New Commons Challenge</h1>
-        <p>The <b>Open Data Policy Lab</b> invites global changemakers to propose innovative data commons for generative AI that serves the public interest. By enhancing data diversity, quality, and provenance, we can unlock AI’s potential to innovate and solve complex challenges.</p>
-        <nc-button el="a" href="apply" color="base" variant="primary">Join the Challenge Today</nc-button>
+        <p>The <b>Open Data Policy Lab</b> invites global changemakers to propose innovative data commons for generative
+          AI that serves the public interest. By enhancing data diversity, quality, and provenance, we can unlock AI’s
+          potential to innovate and solve complex challenges.</p>
+        <nc-button el="a"
+          href="apply"
+          color="base"
+          variant="primary">Join the Challenge Today</nc-button>
         <span>Applications open until <strong>June 2nd.</strong></span>
       </div>
-      <img split-left src="assets/images/hero.jpg" alt="Hero Image">
+      <img split-left
+        src="assets/images/hero.jpg"
+        alt="Hero Image">
     </div>
   </nc-hero>
 
-  <nc-base-section id="video-section" width="narrow">
+  <nc-base-section id="video-section"
+    width="narrow">
     <div class="panel">
       <h2>The Challenge</h2>
-      <p>This video explains what the Challenge is and why data commons are key to developing responsible and effective AI systems.</p>
+      <p>This video explains what the Challenge is and why data commons are key to developing responsible and effective
+        AI systems.</p>
     </div>
     <div class="video-placeholder">Video Section</div>
   </nc-base-section>
-  
+
   <nc-timeline id="timeline" />
 
-  <nc-cta id="cta">
-    <h2>FAQs</h2>
-    <template #right>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </template>
+  <nc-cta id="cta"
+    :single-column="true">
+    <div class="cta-panel"
+      width="narrow">
+      <div class="panel-header">
+        <h2>FAQs</h2>
+        <div>
+          <p>Have questions about the challenge? <br />Check out our FAQ page for all the answers you need to get
+            started!</p>
+          <NuxtLink href="faq">Read More </NuxtLink>
+        </div>
+      </div>
+      <div class="panel-footer">
+        <h2>Watch the New Commons Challenge webinar</h2>
+        <p>Missed the live session? Catch up on our 5 May 2025 webinar to learn key details about the New Commons Challenge, including what we’re looking for and how to strengthen your concept note. We also answered live questions from prospective applicants.</p>
+        <nc-button el="a" color="white" variant="secondary">Watch now!</nc-button>
+      </div>
+    </div>
   </nc-cta>
 
-  <nc-blog-section id="blog" :posts="blogposts"/>
+  <nc-blog-section id="blog"
+    :posts="blogposts" />
 
 </template>
 
@@ -45,7 +68,7 @@ const { data: blogposts } = await useAsyncData('blogposts', () => queryCollectio
   margin-bottom: var(--space-2xl-3xl);
 }
 
-.hero__content > *:first-child {
+.hero__content>*:first-child {
   grid-column: content-start / col6;
 }
 
@@ -71,7 +94,7 @@ const { data: blogposts } = await useAsyncData('blogposts', () => queryCollectio
   font-weight: 400;
 }
 
-.hero__content > *:last-child {
+.hero__content>*:last-child {
   grid-column: col7 / content-end;
 }
 
@@ -99,5 +122,41 @@ const { data: blogposts } = await useAsyncData('blogposts', () => queryCollectio
   justify-content: center;
   font-size: var(--text-l);
   font-weight: 500;
+}
+
+#cta .cta-panel {
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+
+#cta .cta-panel .panel-header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  text-align: left;
+  color: var(--white-color);
+  background: url('./assets/patterns/squares.svg') left top no-repeat, #0E2F40;
+  background-size: contain;
+  border-radius: 8px 8px 0 0;
+  padding: var(--space-2xl-3xl);
+}
+
+#cta .cta-panel .panel-footer {
+  color: var(--white-color);
+  border-radius: 0 0 8px 8px;
+  padding: var(--space-2xl-3xl);
+  background: url('./assets/patterns/waves.png') center center no-repeat, rgba(88, 42, 142, 1);
+  background-blend-mode: darken;
+}
+
+#cta .cta-panel .panel-footer > *:last-child {
+  margin-top: var(--space-l-xl);
+}
+
+#cta .cta-panel .panel-header > * {
+  flex: 1;
 }
 </style>
