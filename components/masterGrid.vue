b00448731757a1608acb040ac7ac88c708f58621
Claudio Mendonca <claudioccm@gmail.com>
2025-06-18 12:33:31 -0700

    grid update

--- components/masterGrid.vue
+++ components/masterGrid.vue
@@ -16,9 +16,36 @@ export default {
 
 <style scoped>
 .master-grid {
+  --column-size: 1fr;
+
+  @media (min-width: 1120px) {
+    --column-size: 5rem;
+  }
+
+  /* max-width: 1120px;
+  margin: 0 auto; */
+}
+
+.master-grid {
   box-sizing: content-box;
   display: grid;
-  grid-template-columns: [full-start] minmax(auto, var(--space-xs-m)) [content-start] 1fr [col1] 1fr [col2] 1fr [col3] 1fr [col4] 1fr [col5] 1fr [col6] 1fr [col7] 1fr [col8] 1fr [col9] 1fr [col10] 1fr [col11] 1fr [col12] 1fr [content-end] minmax(auto, var(--space-xs-m)) [full-end];
+  grid-template-columns: 
+    [full-start] minmax(var(--space-xs-m), 1fr) 
+    [content-start] var(--column-size)
+    [col1]  var(--column-size)
+    [col2]  var(--column-size)
+    [col3]  var(--column-size)
+    [col4]  var(--column-size)
+    [col5]  var(--column-size)
+    [col6]  var(--column-size)
+    [col7]  var(--column-size)
+    [col8]  var(--column-size)
+    [col9]  var(--column-size)
+    [col10] var(--column-size)
+    [col11] var(--column-size)
+    [col12] var(--column-size)
+    [content-end] minmax(var(--space-xs-m), 1fr) 
+    [full-end];
   grid-auto-rows: auto;
   gap: 0;
   min-height: 100svh;
