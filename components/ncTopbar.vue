296fb8dfd1a9649156f75c147d732ecb029d26dc
Seidgy Amano <seidgy@gmail.com>
2025-07-08 09:30:47 -0300

    Event and winner update

--- components/ncTopbar.vue
+++ components/ncTopbar.vue
@@ -7,8 +7,8 @@
       <ul class="topbar__nav | cluster">
         <!-- @TODO: Por algum motivo os NuxtLink não estão funcionando aqui. Só consegui fazer as rotas funcionar com o el="a" -->
         <li><nc-button el="a" href="/prize" color="base" variant="link">The Prize</nc-button></li>
-        <li class="hide-on-mobile"><nc-button el="span" disabled   color="base" variant="link">Event</nc-button></li>
-        <li class="hide-on-mobile"><nc-button el="span" disabled   color="base" variant="link">Winners</nc-button></li>
+        <li><nc-button el="a" href="/event" color="base" variant="link">Event</nc-button></li>
+        <li><nc-button el="a" href="/winners" color="base" variant="link">Winners</nc-button></li>
         <li><nc-button el="a" href="/blog"  color="base" variant="link">Blog</nc-button></li>
         <li><nc-button el="a" href="/faq"   color="base" variant="link">FAQ</nc-button></li>
         <li><nc-button el="a"               color="base" variant="link" href="https://docs.google.com/document/d/1fYmUkwTqOngtpbVT8BezgmWs14IkvGEO6q5a53jqHhA/edit?tab=t.0" target="_blank">Rules <nc-arrow-link-up /></nc-button></li>
