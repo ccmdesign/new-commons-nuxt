5302d0112a8526df4c67cb9bd66afeb4e9200fb6
Claudio Mendonca <claudioccm@gmail.com>
2025-08-06 09:04:39 -0700

    prose fixes

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,12 +1,12 @@
 <template>
-  <nc-base-section width="narrow">
-    <nuxtLink class="button" variant="ghost" to="/blog">← Back to Blog</nuxtLink>
+  <nc-base-section width="narrow" class="blog-post__header | prose">
+    <nuxtLink class="back-button button" variant="ghost" to="/blog">← Back to Blog</nuxtLink>
     <h3 class="brow" v-if="blogpost.brow">{{ blogpost.brow }}</h3>
     <h1>{{ blogpost.heading }}</h1>
     <p class="tagline" v-if="blogpost.tagline">{{ blogpost.tagline }}</p>
   </nc-base-section>
 
-  <nc-base-section width="narrow">
+  <nc-base-section width="narrow" class="blog-post__content | prose">
     <div class="prose">
       <div v-html="blogpost.main_content"></div>  
     </div>
@@ -21,5 +21,5 @@ const { data: blogpost } = await useAsyncData('blogpost', () => queryCollection(
 </script>
 
 <style scoped>
-
+.back-button { margin-left: calc(var(--_button-padding-inline) * -1); }
 </style>
