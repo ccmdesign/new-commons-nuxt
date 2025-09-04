1d257f8209cf0d14240b43892692ed9d89860d8c
Seidgy Amano <seidgy@gmail.com>
2025-09-04 13:19:47 -0300

    Launch checklist

---
+++ pages/404.vue
@@ -0,0 +1,14 @@
+<template> <!-- FOR ERRORS: USE THE error.vue FILE INSTEAD -->
+  <div>
+    <h1>404</h1>
+    <p>Page not found</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
